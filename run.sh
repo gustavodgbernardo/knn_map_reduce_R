a49d7ebb67fd08da4758a9a6a7c085738b7f7b4c
gustavodgbernardo <gustavo.bernardo@gmail.com>
2015-06-22 00:05:11 -0300

    add run
    add run

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-hadoop jar /usr/hdp/2.2.0.0-2041/hadoop-mapreduce/hadoop-streaming-2.6.0.2.2.0.0-2041.jar \
+hadoop jar /usr/hdp/2.2.6.0-2800/hadoop-mapreduce/hadoop-streaming-2.6.0.2.2.6.0-2800.jar \
 -D mapred.map.tasks=2 \
 -D mapred.reduce.tasks=15 \
 -D stream.map.output.field.separator=, \
 -D stream.reduce.input.field.separator=, \
--file /home/rstudio/knn/mapper.R -mapper "Rscript /home/rstudio/knn/mapper.R 5" \
--file /home/rstudio/knn/reducer.R -reducer "Rscript /home/rstudio/knn/reducer.R" \
+-file /tmp/knn_codigos/mapper.R -mapper "Rscript mapper.R 5" \
+-file /tmp/knn_codigos/reducer.R -reducer "Rscript reducer.R" \
 -input /tmp/knn/* -output /tmp/knn2
 
 hadoop fs -cat /tmp/knn2/* > dist_calc.csv
